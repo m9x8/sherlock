54161ad42b0c99c5592369c3a87a6dc57dec4e93
m9x8 <87896713+m9x8@users.noreply.github.com>
2026-08-12 15:09:57 +0200

    Delete tests directory

--- tests/few_test_basic.py
+++
@@ -1,9 +0,0 @@
-import sherlock_project
-
-#from sherlock.sites import SitesInformation
-#local_manifest = data_file_path=os.path.join(os.path.dirname(__file__), "../sherlock/resources/data.json")
-
-from sherlock_interactives import Interactives
-
-def test_username_via_message():
-    assert "Sherlock v" in Interactives.run_cli("--version")
